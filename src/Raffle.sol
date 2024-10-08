ca0c5ea48a19957886e002559d6221abc8b861cc
eason <congmueast@gmail.com>
2024-10-08 23:52:44 +0800

    feat: new enterRaffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,18 +9,39 @@ pragma solidity ^0.8.20;
  * @dev Implements Chainlink VRFv2
   */
 contract Raffle {
-    uint256 private immutable i_entranceFee;
 
-    constructor(uint256 entranceFee) {
+    error Raffle_NotEnoughETHSent(string msg);
+
+    event RaffleEnter(address indexed player);
+
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address[] private s_players;
+
+    constructor(
+        uint256 entranceFee, 
+        uint256 interval
+    ) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
-    
+
 
     function enterRaffle() public payable {
-
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughETHSent("Not Enough ETH Sent!");
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert("Not Enough Time Passed!");
+        }
+    }
 
     function getEntranceFee() external view returns(uint256) {
         return i_entranceFee;
