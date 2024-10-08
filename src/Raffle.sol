263dc347ef13c6942b789311b5f79545cac9e97d
eason <congmueast@gmail.com>
2024-10-09 00:26:35 +0800

    feat: new vrf_middle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.20;
 
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle
  * @author eason
@@ -9,23 +13,36 @@ pragma solidity ^0.8.20;
  * @dev Implements Chainlink VRFv2
   */
 contract Raffle {
-
     error Raffle_NotEnoughETHSent(string msg);
 
     event RaffleEnter(address indexed player);
-
+    
+    uint256 private constant REQUESTION_CONFIRMATIONS = 3;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     uint256 private s_lastTimeStamp;
     address[] private s_players;
 
     constructor(
         uint256 entranceFee, 
-        uint256 interval
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
+
     }
 
 
@@ -41,6 +58,23 @@ contract Raffle {
         if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert("Not Enough Time Passed!");
         }
+
+        // vrf 
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_gasLane,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUESTION_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
     }
 
     function getEntranceFee() external view returns(uint256) {
